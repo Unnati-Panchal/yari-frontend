cdb5cfdb7aa175eb7ff91ba639128691d2a9d697
Dimi <dmitttrov@gmail.com>
2021-02-12 14:03:45 +0200

    added env settings

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API_BASE_URL: 'http://65.0.98.6:8000',
+  thirdPartyVerifyEmail: 'https://verifyemail.invoid.co',
+  thirdPartyPanAndGstVerification: 'https://gc.invoid.co',
+  invoidAuthKey: '894f3c58-895c-45b5-ab29-9ae00b304d53'
+};
