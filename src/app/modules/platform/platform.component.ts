750593e7fbced568a3916996fc8a5294e931e2d7
Dimi <dmitttrov@gmail.com>
2021-02-25 22:44:16 +0200

    my profile image

--- src/app/modules/platform/platform.component.ts
+++ src/app/modules/platform/platform.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '@yaari/services/auth/auth.service';
+import {select, Store} from '@ngrx/store';
+import * as fromAuthSelectors from '~store/auth/auth.selectors';
+import {filter} from 'rxjs/operators';
+import {IAppState} from '~store/app.state';
+import * as fromAuthActions from '~store/auth/auth.actions';
 
 @Component({
   selector: 'app-platform',
@@ -7,10 +12,15 @@ import {AuthService} from '@yaari/services/auth/auth.service';
   styleUrls: ['./platform.component.scss']
 })
 export class PlatformComponent implements OnInit {
+  public supplierDetails$ = this._store.pipe(
+    select(fromAuthSelectors.supplierDetails$),
+    filter(details => !!details)
+  );
 
-  constructor(private _auth: AuthService) { }
+  constructor(private _auth: AuthService, private _store: Store<IAppState>) { }
 
   ngOnInit(): void {
+    this._store.dispatch(fromAuthActions.supplierDetails());
   }
 
   logout(): void {
