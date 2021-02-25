452d7359792bdb0daeb605c13071610da2a66fc0
Dimi <dmitttrov@gmail.com>
2021-02-25 16:56:47 +0200

    registration

--- src/app/modules/auth/registration/components/registration/registration.component.ts
+++ src/app/modules/auth/registration/components/registration/registration.component.ts
@@ -24,10 +24,7 @@ export class RegistrationComponent implements OnInit, OnDestroy {
   public isAuthError$ = this._store.pipe(select(fromAuthSelectors.getIsError), filter(error => !!error));
   public isProductError$ = this._store.pipe(select(fromProductsSelectors.getIsError), filter(error => !!error));
   public categories$ = this._store.pipe(select(fromProductsSelectors.getCategories), filter(categories => !!categories?.length));
-  public panVerification$ = this._store.pipe(select(fromAuthSelectors.panVerification), filter(value => !!value));
-  public gstVerification$ = this._store.pipe(select(fromAuthSelectors.gstVerification), filter(value => !!value));
   public generateOtp$ = this._store.pipe(select(fromAuthSelectors.generateOtp), filter(value => !!value));
-  public verifyOtpResponse$ = this._store.pipe(select(fromAuthSelectors.verifyOtpResponse), filter(value => !!value));
   public onboarders$ = this._store.pipe(select(fromAuthSelectors.onBoarders), filter(onboarders => !!onboarders?.length));
   public regForm: FormGroup;
   public types = [
@@ -38,15 +35,8 @@ export class RegistrationComponent implements OnInit, OnDestroy {
   public categories: ICategory[];
   public loading;
   public loadingEmailVerification: boolean;
-  public loadingGstVerification: boolean;
-  public loadingOtpVerification: boolean;
-  public loadingPanVerification: boolean;
-  public otpVerificationSuccessful: string;
   public emailVerificationSuccessful: string;
-  public gstVerificationSuccessful: string;
-  public gstVerificationFailureReason: string;
-  public panVerificationSuccessful: string;
-  public panVerificationFailureReason: string;
+  errorMsgs: any[];
 
   private _subscription: Subscription = new Subscription();
 
@@ -60,9 +50,6 @@ export class RegistrationComponent implements OnInit, OnDestroy {
     this.initRegistrationForm();
     this.supplierRegistration();
     this.emailVerification();
-    this.otpVerification();
-    this.gstVerification();
-    this.panVerification();
     this._store.dispatch(fromProductsActions.getCategories({categoryId: ''}));
     this._store.dispatch(fromAuthActions.getOnboarders());
   }
@@ -83,30 +70,8 @@ export class RegistrationComponent implements OnInit, OnDestroy {
       this.regForm.get('email_id').setErrors({InvalidValue: true});
       return;
     }
-    if (!this.panVerificationSuccessful) {
-      this.loading = false;
-      this.regForm.get('pan_no').setErrors({InvalidValue: true});
-      return;
-    }
-    if (!this.gstVerificationSuccessful) {
-      this.loading = false;
-      this.regForm.get('gst_no').setErrors({InvalidValue: true});
-      return;
-    }
-    if (!this.gstVerificationSuccessful) {
-      this.loading = false;
-      this.regForm.get('gst_no').setErrors({InvalidValue: true});
-      return;
-    }
-    if (!this.otpVerificationSuccessful) {
-      this.loading = false;
-      this.regForm.get('otp_no').setErrors({InvalidValue: true});
-      return;
-    }
     const regRequest = this.regForm.value;
     regRequest.email_id = this.emailVerificationSuccessful;
-    regRequest.pan_no = this.panVerificationSuccessful;
-    regRequest.gst_no = this.gstVerificationSuccessful;
     this._store.dispatch(fromAuthActions.registration({regRequest}));
   }
 
@@ -117,7 +82,7 @@ export class RegistrationComponent implements OnInit, OnDestroy {
       contact_person: ['', [Validators.required]],
       phone_no: ['', [Validators.required, CustomValidator.digitsOnly]],
       email_id: ['', [Validators.required, Validators.email]],
-      otp_no: ['', [Validators.required]],
+      otp: ['', [Validators.required]],
       city: ['', [Validators.required, CustomValidator.lettersOnly]],
       state: ['', [Validators.required]],
       type: ['', [Validators.required]],
@@ -145,43 +110,6 @@ export class RegistrationComponent implements OnInit, OnDestroy {
           this.regForm.get('gst_no').clearValidators();
         }
         this.regForm.updateValueAndValidity();
-      })
-    );
-  }
-
-  public verifyOtp(event): void {
-    event.preventDefault();
-    this.loadingOtpVerification = true;
-    const email = this.regForm.value.email_id;
-    const otp = this.regForm.value.otp_no;
-    if (!email) {
-      this.regForm.get('email_id').setErrors({required: true});
-      this.loadingOtpVerification = false;
-      return;
-    }
-    if (!otp) {
-      this.regForm.get('otp_no').setErrors({required: true});
-      this.loadingOtpVerification = false;
-      return;
-    }
-    const verifyOtp = {email, otp};
-    this._store.dispatch(fromAuthActions.verifyOtp({verifyOtp}));
-  }
-
-  public otpVerification(): void {
-    this._subscription.add(this.verifyOtpResponse$.subscribe((registered) => {
-        this.loadingOtpVerification = false;
-        if (registered.message === 'Success') {
-          this.otpVerificationSuccessful = registered.email_id;
-        } else {
-          this.otpVerificationSuccessful = '';
-          this.regForm.get('otp_no').setErrors({InvalidValue: true});
-        }
-      },
-      () => {
-        this.loadingOtpVerification = false;
-        this.regForm.get('otp_no').setErrors({InvalidValue: true});
-        this.otpVerificationSuccessful = '';
       })
     );
   }
@@ -216,69 +144,15 @@ export class RegistrationComponent implements OnInit, OnDestroy {
     );
   }
 
-  public verifyGst(event): void {
-    event.preventDefault();
-    this.loadingGstVerification = true;
-    const gstReq = {
-      gst_no: this.regForm.value.gst_no,
-      name: this.regForm.value.contact_person
-    };
-    this._store.dispatch(fromAuthActions.gstVerification({gstReq}));
-  }
-
-  public gstVerification(): void {
-    this._subscription.add(this.gstVerification$.subscribe((registered) => {
-        this.loadingGstVerification = false;
-        if (registered.details === 'GST Verified') {
-          this.gstVerificationSuccessful = registered.gst_no;
-        } else {
-          this.gstVerificationSuccessful = '';
-          this.regForm.get('gst_no').setErrors({InvalidValue: true});
-          this.gstVerificationFailureReason = registered.details;
-        }
-      },
-      (error) => {
-        this.loadingGstVerification = false;
-        this.regForm.get('gst_no').setErrors({InvalidValue: true});
-        this.gstVerificationSuccessful = '';
-        this.gstVerificationFailureReason = error.details;
-      })
-    );
-  }
-
-  public verifyPan(event): void {
-    event.preventDefault();
-    this.loadingPanVerification = true;
-    const panReq = {
-      pan_no: this.regForm.value.pan_no,
-      name: this.regForm.value.contact_person
-    };
-    this._store.dispatch(fromAuthActions.panVerification({panReq}));
-  }
-
-  public panVerification(): void {
-    this._subscription.add(this.panVerification$.subscribe((registered) => {
-        this.loadingPanVerification = false;
-        if (registered.details === 'PAN Verified') {
-          this.panVerificationSuccessful = registered.pan_no;
-        } else {
-          this.panVerificationSuccessful = '';
-          this.regForm.get('pan_no').setErrors({InvalidValue: true});
-          this.panVerificationFailureReason = registered.details;
-        }
-      },
-      (error) => {
-        this.loadingPanVerification = false;
-        this.regForm.get('pan_no').setErrors({InvalidValue: true});
-        this.panVerificationSuccessful = '';
-        this.panVerificationFailureReason = error.details;
-      })
-    );
-  }
-
   public supplierRegistration(): void {
-    this._subscription.add(this.isAuthError$.subscribe(error => console.log(error)));
-    this._subscription.add(this.isProductError$.subscribe(error => console.log(error)));
+    this._subscription.add(this.isAuthError$.subscribe((error: any) => {
+      this.errorMsgs = error.detail;
+      this.loading = false;
+    }));
+    this._subscription.add(this.isProductError$.subscribe((error: any) => {
+      this.errorMsgs = error.detail;
+      this.loading = false;
+    }));
     this._subscription.add(this.registrationResponse$.subscribe(() => {
       this.loading = false;
       this._router.navigate(['app/dashboard']);
