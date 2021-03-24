65905577684b18dc6070fe72d416edbe43c6ef5e
Dimi <dmitttrov@gmail.com>
2021-03-24 14:38:30 +0200

    fixed pickup address

--- src/app/modules/platform/profile/components/pickup-address/pickup-address.component.ts
+++ src/app/modules/platform/profile/components/pickup-address/pickup-address.component.ts
@@ -1,16 +1,16 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
+import {NavigationStart, Router} from '@angular/router';
 import {Subscription} from 'rxjs';
 import {filter} from 'rxjs/operators';
 import {select, Store} from '@ngrx/store';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 import {IAppState} from '~store/app.state';
 import * as fromProfileSelectors from '~store/profile/profile.selectors';
 import {CustomValidator} from '@yaari/utils/custom-validators';
 import * as fromProfileActions from '~store/profile/profile.actions';
 import {IPickupAddress} from '@yaari/models/profile/profile.interface';
-
 
 @Component({
   selector: 'app-pickup-address',
@@ -23,12 +23,14 @@ export class PickupAddressComponent implements OnInit, OnDestroy {
   public regForm: FormGroup;
   public loading;
   public submitted: boolean;
+  public address: IPickupAddress;
 
   private _subscription: Subscription = new Subscription();
 
   constructor(private _store: Store<IAppState>,
               private _formBuilder: FormBuilder,
-              private _router: Router
+              private _router: Router,
+              private _snackBar: MatSnackBar
   ) {
   }
 
@@ -73,7 +75,9 @@ export class PickupAddressComponent implements OnInit, OnDestroy {
 
   public pickupAddressSub(): void {
     this._subscription.add(this.isError$.subscribe(() => this.loading = false));
+
     this._subscription.add(this.isPickupAddress$.subscribe(address => {
+      this.address = address;
       this.regForm.get('full_name').patchValue(address?.full_name);
       this.regForm.get('phone_no').patchValue(address?.phone_no);
       this.regForm.get('country').patchValue(address?.country);
@@ -85,18 +89,21 @@ export class PickupAddressComponent implements OnInit, OnDestroy {
       this.regForm.get('state').patchValue(address?.state);
       this.regForm.get('type').patchValue(address?.type);
       this.loading = false;
-      if (address && this.submitted) {
-        this._store.dispatch(fromProfileActions.getPickupAddress());
+      if (address?.full_name && this.submitted) {
+        // this._store.dispatch(fromProfileActions.getPickupAddress());
         this._router.navigate(['app/dashboard']);
         this.submitted = false;
       }
     }));
 
-    // this.isPickupAddress$.subscribe((address) => {
-    //   if (!address) {
-    //     this._router.navigate(['app/profile/pickup-address']);
-    //     return true;
-    //   }
-    // });
+    this._subscription.add(
+      this._router.events.pipe(filter(val => val instanceof NavigationStart))
+        .subscribe((val: NavigationStart) => {
+        if (val.url !== '/auth/login' && !this.address?.full_name) {
+          this._snackBar.open('Please enter your pickup address!', '', {duration: 3000});
+          this._router.navigate(['app/profile/pickup-address']);
+        }
+      })
+    );
   }
 }
