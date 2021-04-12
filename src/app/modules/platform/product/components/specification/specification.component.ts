57083fedfdb699e06b1a183749a6ebacf5490691
Dimi <dmitttrov@gmail.com>
2021-04-12 21:24:00 +0300

    YARI-378

--- src/app/modules/platform/product/components/specification/specification.component.ts
+++ src/app/modules/platform/product/components/specification/specification.component.ts
@@ -88,12 +88,13 @@ export class SpecificationComponent implements OnInit, OnDestroy {
     this._subscription.add(
       combineLatest([this.getCatalogProducts$, this.getSpecTemplate$])
         .subscribe(([catalogueProducts, specTemplate]) => {
-        this.specKeys = [...new Set(specTemplate)]; // Object.keys(catalogueProducts[0]?.specifications);
+        this.specKeys = [...new Set(specTemplate)];
         this.displayedColumns = ['sr_no', 'sku_id', 'product_name', ...this.specKeys, 'next_day_dispatch'];
         this.dataSource = catalogueProducts.map( (item) => {
+          const specDetails = this.specKeys.map( key => item.specifications[key]);
           return {
             ...item,
-            specDetails: item?.specifications ? Object.values(item.specifications) : []
+            specDetails: item?.specifications ? specDetails : []
           };
         });
         this.loading = false;
