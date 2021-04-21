25fe7be63a506f4e4f3b956eb2c2401434b2e007
Dimi <dmitttrov@gmail.com>
2021-04-21 10:39:35 +0300

    new prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'https://api.yaari.com/api/v1/docs',
+  API_BASE_URL: 'https://api.yaari.com',
   thirdPartyVerifyEmail: 'https://verifyemail.invoid.co',
   thirdPartyPanAndGstVerification: 'https://gc.invoid.co',
   invoidAuthKey: '894f3c58-895c-45b5-ab29-9ae00b304d53'
