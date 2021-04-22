c33ed99dc82ef24e31a56780fb47123a5ce11c0b
Dimi <dmitttrov@gmail.com>
2021-04-22 12:59:15 +0300

    new env endpoints
    (cherry picked from commit 0ed8a7e2b1d4a652e87a08a1826c446923b61003)

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://65.1.182.179:8000',
+  // API_BASE_URL: 'http://65.1.182.179:8000',
+  API_BASE_URL: ' https://api-dev.yaari.com',
   thirdPartyVerifyEmail: 'https://verifyemail.invoid.co',
   thirdPartyPanAndGstVerification: 'https://gc.invoid.co',
   invoidAuthKey: '894f3c58-895c-45b5-ab29-9ae00b304d53'
