0ed8a7e2b1d4a652e87a08a1826c446923b61003
Dimi <dmitttrov@gmail.com>
2021-04-22 12:59:15 +0300

    new env endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://13.127.136.69:8000',
-  // API_BASE_URL: 'https://api.yaari.com',
+  // API_BASE_URL: 'http://13.127.136.69:8000',
+  API_BASE_URL: 'https://api.yaari.com',
   thirdPartyVerifyEmail: 'https://verifyemail.invoid.co',
   thirdPartyPanAndGstVerification: 'https://gc.invoid.co',
   invoidAuthKey: '894f3c58-895c-45b5-ab29-9ae00b304d53'
