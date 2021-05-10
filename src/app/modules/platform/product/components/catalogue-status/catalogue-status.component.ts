6140525c2799a2e4f58381ab512bf6a2cf1986fd
Dimi <dmitttrov@gmail.com>
2021-05-10 23:45:48 +0300

    681

--- src/app/modules/platform/product/components/catalogue-status/catalogue-status.component.ts
+++ src/app/modules/platform/product/components/catalogue-status/catalogue-status.component.ts
@@ -24,7 +24,7 @@ export class CatalogueStatusComponent implements OnInit, OnDestroy {
     start: new FormControl(),
     end: new FormControl()
   });
-  displayedColumns: string[] = ['sr_no', 'catalog_name', 'category_name', 'created_time', 'approved', 'viewed', 'shared'];
+  displayedColumns: string[] = ['sr_no', 'catalog_name', 'category_name', 'created_time', 'approved', 'viewed', 'shared', 'comment'];
   selectedDate: IQuery;
   private _subscription: Subscription = new Subscription();
   public getCatalogues$ = this._store.pipe(select(fromProductsSelectors.getCatalogs), filter(catalogs => !!catalogs));
