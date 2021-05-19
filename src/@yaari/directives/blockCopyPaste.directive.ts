1c6010a07290c45c4a00bcc7a0ef8e1ca7113570
Dimi <dmitttrov@gmail.com>
2021-05-19 14:36:06 +0300

    709

--- src/@yaari/directives/blockCopyPaste.directive.ts
+++ src/@yaari/directives/blockCopyPaste.directive.ts
@@ -1,20 +1,22 @@
 import { Directive, HostListener } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Directive({
   selector: '[appBlockCopyPaste]'
 })
 export class BlockCopyPasteDirective {
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar) { }
 
-  @HostListener('paste', ['$event']) blockPaste(e: KeyboardEvent) {
+  @HostListener('paste', ['$event']) blockPaste(e: KeyboardEvent): void {
+    this._snackBar.open(`You're not allowed to paste in this field`, '', { duration: 3000 });
     e.preventDefault();
   }
 
-  @HostListener('copy', ['$event']) blockCopy(e: KeyboardEvent) {
+  @HostListener('copy', ['$event']) blockCopy(e: KeyboardEvent): void {
     e.preventDefault();
   }
 
-  @HostListener('cut', ['$event']) blockCut(e: KeyboardEvent) {
+  @HostListener('cut', ['$event']) blockCut(e: KeyboardEvent): void {
     e.preventDefault();
   }
 
@@ -28,8 +30,8 @@ export class BlockCopyPasteDirective {
 
   @HostListener('keydown', ['$event'])
   public onKeydownHandler(e: KeyboardEvent): void {
-    if(e.keyCode===13){
-      alert("enter")
+    if(e.keyCode === 13){
+      alert('enter');
     }
   }
 }
