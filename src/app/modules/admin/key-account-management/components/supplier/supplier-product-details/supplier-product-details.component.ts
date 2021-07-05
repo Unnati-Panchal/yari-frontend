98e48249f034a769bc3cc961a76f927ef003a6d2
Dimi <dmitttrov@gmail.com>
2021-07-05 12:32:58 +0300

    1274

--- src/app/modules/admin/key-account-management/components/supplier/supplier-product-details/supplier-product-details.component.ts
+++ src/app/modules/admin/key-account-management/components/supplier/supplier-product-details/supplier-product-details.component.ts
@@ -49,7 +49,7 @@ export class SupplierProductDetailsComponent implements OnInit, OnDestroy {
 
   public viewBtn(): void {
     const query: IFilter = {
-      filterBy: this.selectedName.trim()
+      filterBy: this.selectedName?.trim()
     };
     this.loading = true;
     this.submitted = true;
